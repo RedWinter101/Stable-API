0e31b15d47576e54f010bd32883482997fe4b23a
RedWinter101 <124126962+RedWinter101@users.noreply.github.com>
2023-04-18 13:59:41 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import {
   Image,
   SkeletonCircle,
   SkeletonText,
+
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useState } from "react";
@@ -18,9 +19,9 @@ const App = () => {
   const [image, updateImage] = useState();
   const [prompt, updatePrompt] = useState("");
   const [Nprompt, updateNPrompt] = useState("");
-  const [inference, updateSamplingSteps] = useState();
-  const [width, updateWidth ]= useState();
-  const [height, updateHeight] = useState();
+  const [inference, updateSamplingSteps] = useState("");
+  const [width, updateWidth ]= useState("");
+  const [height, updateHeight] = useState("");
   const [loading, updateLoading] = useState();
 
   const generate = async () => {
@@ -33,62 +34,74 @@ const App = () => {
 
   return (
     <ChakraProvider>
-      <Container>
-        <Heading>Stable DIffusion🖼️</Heading>
-        <Text marginBottom={"10px"}>
+      <Container boxShadow={"lg"}>
+        <Heading textAlign={'center'}>Stable DIffusion🖼️</Heading>
+        <Text marginBottom={"10px"} textAlign={'justify'}>
           This react application leverages the model trained by SG161222
           to generate images using the Stable Diffusion Deep Learning
-          model.
+          model. 
         </Text>
-        <Wrap marginBottom={"10px"}>
+        <Wrap marginBottom={"10px"} >
           <Input
+            left={'10px'}
             value={prompt}
+            variant={'filled'}
             placeholder="Prompt"
             onChange={(e) => updatePrompt(e.target.value)}
             width={"350px"}
           ></Input>
-          <Button onClick={(e) => generate()} colorScheme={"blue"} isDisabled={!prompt + !Nprompt + !inference + !width + !height}>
+          <Button onClick={(e) => generate()} colorScheme={"blue"} 
+          isDisabled={!prompt + !Nprompt + !inference + !width + !height}
+          left={'20px'}
+          >
             Generate
           </Button>
           <Input
+            left={'10px'}
             value={Nprompt}
+            variant={'filled'}
             placeholder="Negative Prompt"
             onChange={(e) => updateNPrompt(e.target.value)}
             width={"350px"}
           ></Input>
         </Wrap>
 
+        <Wrap className="btn" marginBottom={"10px"}>
+          <Input 
+            left={'10px'}
+            margin={"5px"}
+            value={inference}
+            placeholder="Steps"
+            onChange={(e) => updateSamplingSteps(e.target.value)}
+            width={"110px"} 
+          ></Input>
+          <Input
+            left={'10px'}
+            margin={"5px"}
+            value={width}
+            placeholder="Width"
+            onChange={(e) => updateWidth(e.target.value)}
+            width={"110px"} 
+          ></Input>
+          <Input
+            left={'10px'}
+            margin={"5px"}
+            value={height}
+            placeholder="Height"
+            onChange={(e) => updateHeight(e.target.value)}
+            width={"110px"} 
+          ></Input>
+        </Wrap>
+        <Wrap boxShadow={'lg'} height={'450px'} margin={'5px'} marginBottom={'10px'}>
         {loading ? (
           <Stack>
             <SkeletonCircle />
             <SkeletonText />
           </Stack>
         ) : image ? (
+          //<Image src={require("./lgs/download.png")} boxShadow="lg" />
           <Image src={`data:image/png;base64,${image}`} boxShadow="lg" />
         ) : null}
-        
-        <Wrap className="btn" marginTop={"10px"}>
-          <Input 
-            margin={"5px"}
-            value={inference}
-            placeholder="Steps"
-            onChange={(e) => updateSamplingSteps(e.target.value)}
-            width={"105px"} 
-          ></Input>
-          <Input
-            margin={"5px"}
-            value={width}
-            placeholder="Width"
-            onChange={(e) => updateWidth(e.target.value)}
-            width={"105px"} 
-          ></Input>
-          <Input
-            margin={"5px"}
-            value={height}
-            placeholder="Height"
-            onChange={(e) => updateHeight(e.target.value)}
-            width={"105px"} 
-          ></Input>
         </Wrap>
       </Container>
     </ChakraProvider>
@@ -96,4 +109,3 @@ const App = () => {
 };
 
 export default App;
-
